99d75b8abc0ef86d9371eb03e569a23e68a66040
msimp <max.simpson@gmail.com>
2021-08-23 18:41:04 -0500

    deleted incorrect index.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express  = require('express');
 const app = express();
+const path = require('path');
 
-app.get()
-app.post()
-app.put()
-app.delete()
+
+const PORT = process.env.PORT || 3001;
+app.listen(3001, () => console.log(`listening to port ${PORT}`));
 
 app.get('/', (req, res) => {
     res.send('hello');
 })
 
-const PORT = process.env.PORT || 3001;
-app.listen(3001, () => console.log(`listening to port ${PORT}`));
+
+
+
+
+
+
+// app.get()
+// app.post()
+// app.put()
+// app.delete()
