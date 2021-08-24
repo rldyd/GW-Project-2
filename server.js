defbc0d084c9992fadf414e4a8ad718d9026ea53
msimp <max.simpson@gmail.com>
2021-08-23 19:17:32 -0500

    created additional files

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express  = require('express');
 const app = express();
 const path = require('path');
+const { Sequelize } = require('sequelize');
 
+
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('pets', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+    .then (() => console.log('connected!'))
+    .catch (err => console.log('error' + err));
 
 const PORT = process.env.PORT || 3001;
 app.listen(3001, () => console.log(`listening to port ${PORT}`));
