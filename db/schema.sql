315123558c2ff790680dfc40945a34431657c540
rldyd <kyleahn89@gmail.com>
2021-09-01 20:39:52 -0400

    user routes working except pets

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,3 @@
-
-
 DROP DATABASE IF EXISTS petfinder_db;
 
 -- CREATE DATABASE
--- seeds/index.js
+++ seeds/index.js
@@ -4,15 +4,15 @@ const seedUsers = require('./user-seeds');
 const sequelize = require("../config/connection");
 
 const seedAll = async () => {
-    await sequelize.sync({ force: true });
-    console.log("--------------");
-    await seedUsers();
-    console.log("--------------");
-  
-    await seedPets();
-    console.log("--------------");
-  
-    process.exit(0);
-  };
-  
-  seedAll();
+  await sequelize.sync({ force: true });
+  console.log("--------------");
+  await seedUsers();
+  console.log("--------------");
+
+  await seedPets();
+  console.log("--------------");
+
+  process.exit(0);
+};
+
+seedAll();
