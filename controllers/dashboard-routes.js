834f794869bbae3e8a2f51cdd244bfc635fb4f35
Maxdamoe <max.simpson@gmail.com>
2021-09-09 13:09:43 +0200

    added edit and delete functionality

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -36,45 +36,40 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
-// router.get('/edit/:id', withAuth, (req, res) => {
-//   Post.findByPk(req.params.id, {
-//     attributes: [
-//       'id',
-//       'post_url',
-//       'title',
-//       'created_at',
+// EDIT PET FROM DASHBOARD
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Pet.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'pet_name',
+      'pet_age',
+      'pet_type',
+      'pet_health'
       
-//     ],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//         include: {
-//           model: User,
-//           attributes: ['username']
-//         }
-//       },
-//       {
-//         model: User,
-//         attributes: ['username']
-//       }
-//     ]
-//   })
-//     .then(dbPostData => {
-//       if (dbPostData) {
-//         const post = dbPostData.get({ plain: true });
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
         
-//         res.render('edit-post', {
-//           post,
-//           loggedIn: true
-//         });
-//       } else {
-//         res.status(404).end();
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).json(err);
-//     });
-// });
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,23 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const pet_name = document.querySelector('input[name="pet-name"]').value.trim();
+  const pet_age = document.querySelector('input[name="pet-age"]').value.trim();
+  const pet_type = document.querySelector('input[name="pet-type"]').value.trim();
+  const pet_health = document.querySelector('input[name="pet-health"]').value.trim();
+
+
+
   const id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
     method: "PUT",
     body: JSON.stringify({
-      title,
+      pet_name,
+      pet_age,
+      pet_type,
+      peet_health,
     }),
     headers: {
       "Content-Type": "application/json",
